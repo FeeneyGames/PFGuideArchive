a594d348dfc5228f3539386b5a056f173be7a279
FeeneyGames <patrickfeeneygames@gmail.com>
2021-01-18 20:21:31 -0500

    Metadata and URL Parsing

--- gdrive.py
+++ gdrive.py
@@ -13,7 +13,7 @@ class DriveDownloader():
         self.service = build("drive", "v3", credentials=creds)
 
     def download_doc(self, file_id):
-        """Download a Google Doc from the URL
+        """Download a Google Doc from the file id
 
         Args:
             file_id (str): ID for the Google Doc
@@ -24,9 +24,18 @@ class DriveDownloader():
         Returns:
             io.BytesIO: Binary stream representing the file
         """
+        # determine correct mimeType
+        cur_type = self.get_doc_type(file_id)
+        type_map = {
+            "application/pdf": "application/pdf",
+            "application/vnd.google-apps.document": "application/zip"
+        }
+        if cur_type not in type_map:
+            raise ValueError("File ID identifies document with unhandled type:\n" + cur_type)
+        else:
+            download_type = type_map[cur_type]
         # construct request to Google Docs
-        # TODO determine correct mimeType
-        request = self.service.files().export_media(fileId=file_id, mimeType='text/html')
+        request = self.service.files().export_media(fileId=file_id, mimeType=download_type)
         # construct buffer and downloader
         file_buffer = io.BytesIO()
         downloader = MediaIoBaseDownload(file_buffer, request)
@@ -34,10 +43,51 @@ class DriveDownloader():
         done = False
         while done is False:
             status, done = downloader.next_chunk()
-            print("Download %d%%." % int(status.progress() * 100))
         return file_buffer
 
-    def get_doc_type(self, file_id):
+    def get_doc_metadata(self, file_id):
         # gets default metadata fields
         response_dict = self.service.files().get(fileId=file_id).execute()
-        return response_dict["mimeType"]
+        return response_dict
+
+    def get_doc_type(self, file_id):
+        return self.get_doc_metadata(file_id)["mimeType"]
+
+    def get_doc_name(self, file_id):
+        return self.get_doc_metadata(file_id)["name"]
+
+    def url_to_file_id(self, url):
+        if not (url.startswith("https://docs.google.com") or
+                url.startswith("https://drive.google.com")):
+            raise ValueError("Unexpected URL domain")
+        if url.startswith("https://docs.google.com/spreadsheet/"):
+            raise ValueError("Spreadsheets not yet handled")
+        file_id = None
+        if "/d/" in url:
+            start_ind = url.find("/d/")
+            if start_ind > -1:
+                start_ind += 3
+                file_id = url[start_ind:]
+                end_ind = file_id.find("/")
+                if end_ind > -1:
+                    file_id = file_id[:end_ind]
+        else:
+            fields = ["id", "docid", "srcid"]
+            for field in fields:
+                if "?" + field + "=" in url or \
+                   "&" + field + "=" in url:
+                    file_id = self.extract_url_query_field(url, field)
+                    break
+        if file_id is None:
+            raise ValueError("URL does not match known patterns:\n" + url)
+        if file_id.endswith("#"):
+            file_id[:-1]
+        return file_id
+
+    def extract_url_query_field(self, url, field):
+        start_ind = url.find(field + "=") + len(field) + 1
+        field = url[start_ind:]
+        end_ind = field.find("&")
+        if end_ind > -1:
+            field = field[:end_ind]
+        return field
