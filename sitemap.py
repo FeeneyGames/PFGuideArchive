39727eedf68da959f743dc082bc0552a7b125466
FeeneyGames <patrickfeeneygames@gmail.com>
2021-04-05 23:06:33 -0400

    Sitemap Generator

---
+++ sitemap.py
@@ -0,0 +1,13 @@
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+
+
+site_url = "https://feeneygames.github.io/PFGuideArchive/"
+with open("archive_links.html", encoding="utf8") as f:
+    soup = BeautifulSoup(f, "html.parser")
+with open("sitemap.txt", "w", encoding="utf8") as f:
+    for tag in soup.find_all("a"):
+        url = tag["href"].replace("\\", "/")
+        url = urljoin(site_url, url)
+        f.write(url + "\n")
