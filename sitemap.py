b88a1ba89e9d8f4f8eb532a153c237d146d3ccad
FeeneyGames <patrickfeeneygames@gmail.com>
2021-04-05 23:15:04 -0400

    Add Root to Sitemap

--- sitemap.py
+++ sitemap.py
@@ -7,6 +7,7 @@ site_url = "https://feeneygames.github.io/PFGuideArchive/"
 with open("archive_links.html", encoding="utf8") as f:
     soup = BeautifulSoup(f, "html.parser")
 with open("sitemap.txt", "w", encoding="utf8") as f:
+    f.write(site_url + "\n")
     for tag in soup.find_all("a"):
         url = tag["href"].replace("\\", "/")
         url = urljoin(site_url, url)
